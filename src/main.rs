af23dccbd0cb5f11f3bb0585301fd6f178d85503
jurco-exe <jurajbosy1@gmail.com>
2024-02-14 17:11:06 +0100

    feat: nhehehehehehe

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use actix_web::{web, App, HttpServer, HttpResponse};
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello, Actix!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(web::resource("/").to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
